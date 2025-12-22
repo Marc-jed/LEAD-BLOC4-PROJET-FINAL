6634dccf3983b09ed74331ebe97758b5ba2d8cae
Marc <gdleds31@gmail.com>
2025-12-22 22:41:48 +0100

    Update test_coord_station.py

--- tests/test_coord_station.py
+++ tests/test_coord_station.py
@@ -7,6 +7,6 @@ def test_load_coord_station(coord_station):
         assert coord_station[col].isnull().sum() == 0,  f"NaN détecté dans la colonne {col}"
 
 def test_lat_lon_ranges(coord_station):
-    assert coord_station["latitude"].between(41, 44).all()
-    assert coord_station["longitude"].between(8, 10).all()
+    assert coord_station["lat"].between(41, 44).all()
+    assert coord_station["lon"].between(8, 10).all()
 
