ed127967374df0f8af1b2369be0e7661f3c163c1
Marc <gdleds31@gmail.com>
2025-12-22 22:56:04 +0100

    Update test_histo_feu.py

--- tests/test_histo_feu.py
+++ tests/test_histo_feu.py
@@ -3,6 +3,5 @@ import pytest
 
 def test_data_feu(historique_feu):
     assert historique_feu.shape[1] == 24, "Le fichier feu doit avoir 24 colonnes"
-    assert historique_feu['Feux'].isin([1]).all(), f'il y a une valeur différente de 1'
     for col in historique_feu.columns[:5]:
         assert historique_feu[col].isnull().sum() == 0, f"NaN détecté dans la colonne {col}"
