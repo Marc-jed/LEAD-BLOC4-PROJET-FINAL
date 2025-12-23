6fa0fb81abdf99be19317245bc8b03c6941ef0cc
Marc <gdleds31@gmail.com>
2025-12-23 09:20:20 +0100

    Update test_histo_feu.py

--- tests/test_histo_feu.py
+++ tests/test_histo_feu.py
@@ -2,7 +2,8 @@ import pandas as pd
 import pytest
 
 def test_data_feu(historique_feu):
-    assert historique_feu.shape[1] == 24, "Le fichier feu doit avoir 24 colonnes"
+    assert historique_feu.shape[1] == 19, "Le fichier feu doit avoir 24 colonnes"
+    assert historique_feu['Feux'].isin([1]).all(), f'il y a une valeur différente de 1'
     for col in historique_feu.columns[:5]:
         nan_ratio = historique_feu[col].isnull().mean()
         assert nan_ratio < 0.01, f"Trop de NaN dans la colonne {col} ({nan_ratio:.2%})"
