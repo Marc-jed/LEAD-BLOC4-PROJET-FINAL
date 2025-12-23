9121c0e8ce7339899a44351caffcb1b84855b0d2
Marc <gdleds31@gmail.com>
2025-12-23 10:42:42 +0100

    Delete tests/test_pred_data.py

--- tests/test_pred_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import pytest
-
-def data_prediction(prediction_get):
-    assert prediction_get.shape[1] == 75, "Le dataset final doit avoir 75 colonnes"
-    assert prediction_get["évènement"].dtypes == bool
-    assert prediction_get["Feu_prévu"].dtypes == bool
-    assert (prediction_get["décompte"].dropna() >=0).all(), "la colonne décompte doit contenir un chiffre positif ou 0"
