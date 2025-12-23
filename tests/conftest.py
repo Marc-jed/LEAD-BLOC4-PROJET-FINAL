414638c031e2806c6752662ffc179cd9df7a1889
Marc <gdleds31@gmail.com>
2025-12-23 09:19:34 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,7 +79,7 @@ def coord_station(s3_client, tmp_path_factory):
 @pytest.fixture(scope="session")
 def historique_feu(s3_client, tmp_path_factory):
     bucket = os.environ["S3_BUCKET_NAME"]
-    s3_key = "dataset/Incendies_2006_2024 (1).csv"
+    s3_key = "dataset/historique_incendie_corse_cleaned_2025.csv"
     
 
     tmp_dir = tmp_path_factory.mktemp("s3_data")
@@ -87,7 +87,7 @@ def historique_feu(s3_client, tmp_path_factory):
 
     s3_client.download_file(bucket, s3_key, str(local_file))
 
-    df = pd.read_csv(local_file, sep=";", low_memory=False)
+    df = pd.read_csv(local_file, sep=",", low_memory=False)
 
     if df.empty:
         pytest.fail("Downloaded historique incendie dataset is empty")
